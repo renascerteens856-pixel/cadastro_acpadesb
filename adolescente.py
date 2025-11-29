b5417062514d6f72e0eaecf6e3c96f3b85c3f7c3
renascerteens856-pixel <renascerteens856@gmail.com>
2025-11-29 11:55:14 -0300

    Update adolescente.py

--- adolescente.py
+++ adolescente.py
@@ -1,10 +1,8 @@
-from dataclasses import dataclass
-from typing import Optional, Dict, Any
-
-@dataclass
 class Adolescente:
-    nome: str
-    idade: int
-    nome_pai: Optional[str] = None
-    nome_mae: Optional[str] = None
-    endereco: Dict[str, Any] = None
+    def __init__(self, nome, nome_pai=None, nome_mae=None, endereco=None, contato=None, data_nasc=None):
+        self.nome = nome
+        self.nome_pai = nome_pai
+        self.nome_mae = nome_mae
+        self.endereco = endereco
+        self.contato = contato
+        self.data_nasc = data_nasc
