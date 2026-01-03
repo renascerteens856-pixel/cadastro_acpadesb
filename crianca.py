f48a4bf382709f3801b9bb3bbd99d961ae129b3f
renascerteens856-pixel <renascerteens856@gmail.com>
2026-01-03 10:10:18 -0300

    Create crianca.py

---
+++ crianca.py
@@ -0,0 +1,9 @@
+class Crianca:
+    def __init__(self, nome, nome_pai=None, nome_mae=None, endereco=None, contato=None, data_nasc=None, congregacao=None):
+        self.nome = nome
+        self.nome_pai = nome_pai
+        self.nome_mae = nome_mae
+        self.endereco = endereco
+        self.contato = contato
+        self.data_nasc = data_nasc
+        self.congregacao = congregacao
